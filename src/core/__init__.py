b92bb16e79564330fc07459edfd2f8ac9ea24c6d
gakkiyomi <gakkiyomi@aliyun.com>
2024-12-22 13:11:38 +0800

    optimze: 优化代码

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -89,44 +89,52 @@ class CilConfigInitor(Initor):
         just_fix_windows_console()
 
 
+def _login_with_key(api: FishPi) -> None:
+    username = api.user.get_username_by_key(
+        GLOBAL_CONFIG.auth_config.key)
+    if username is not None:
+        GLOBAL_CONFIG.auth_config.username = username
+        api.set_token(GLOBAL_CONFIG.auth_config.key)
+        api.current_user = username
+    else:
+        print("非法API-KEY, 使用账户密码登陆")
+        while len(GLOBAL_CONFIG.auth_config.username) == 0:
+            print('请输入用户名:')
+            GLOBAL_CONFIG.auth_config.username = input("")
+        while len(GLOBAL_CONFIG.auth_config.password) == 0:
+            print('请输入密码:')
+            GLOBAL_CONFIG.auth_config.password = input("")
+        api.login(GLOBAL_CONFIG.auth_config.username,
+                  GLOBAL_CONFIG.auth_config.password,
+                  GLOBAL_CONFIG.auth_config.mfa_code)
+        GLOBAL_CONFIG.auth_config.key = api.api_key
+        if cli_login(GLOBAL_CONFIG.auth_config.username):
+            api.user_key_write_to_config_file()
+
+
+def _login_without_key(api: FishPi) -> None:
+    while len(GLOBAL_CONFIG.auth_config.username) == 0:
+        print('请输入用户名:')
+        GLOBAL_CONFIG.auth_config.username = input("")
+    while len(GLOBAL_CONFIG.auth_config.password) == 0:
+        print('请输入密码:')
+        GLOBAL_CONFIG.auth_config.password = input("")
+    api.login(GLOBAL_CONFIG.auth_config.username,
+              GLOBAL_CONFIG.auth_config.password,
+              GLOBAL_CONFIG.auth_config.mfa_code)
+    GLOBAL_CONFIG.auth_config.key = api.api_key
+    if cli_login(GLOBAL_CONFIG.auth_config.username):
+        api.user_key_write_to_config_file()
+
+
 class LoginInitor(Initor):
     def exec(self, api: FishPi, options: CliOptions) -> None:
         os.environ['NO_PROXY'] = GLOBAL_CONFIG.host
         if GLOBAL_CONFIG.auth_config.key == '':
-            while len(GLOBAL_CONFIG.auth_config.username) == 0:
-                print('请输入用户名:')
-                GLOBAL_CONFIG.auth_config.username = input("")
-            while len(GLOBAL_CONFIG.auth_config.password) == 0:
-                print('请输入密码:')
-                GLOBAL_CONFIG.auth_config.password = input("")
-            api.login(GLOBAL_CONFIG.auth_config.username,
-                      GLOBAL_CONFIG.auth_config.password,
-                      GLOBAL_CONFIG.auth_config.mfa_code)
-            GLOBAL_CONFIG.auth_config.key = api.api_key
-            if cli_login(GLOBAL_CONFIG.auth_config.username):
-                api.user_key_write_to_config_file()
+            _login_without_key(api)
         else:
             # 直接使用api-key
-            username = api.user.get_username_by_key(
-                GLOBAL_CONFIG.auth_config.key)
-            if username is not None:
-                GLOBAL_CONFIG.auth_config.username = username
-                api.set_token(GLOBAL_CONFIG.auth_config.key)
-                api.current_user = username
-            else:
-                print("非法API-KEY, 使用账户密码登陆")
-                while len(GLOBAL_CONFIG.auth_config.username) == 0:
-                    print('请输入用户名:')
-                    GLOBAL_CONFIG.auth_config.username = input("")
-                while len(GLOBAL_CONFIG.auth_config.password) == 0:
-                    print('请输入密码:')
-                    GLOBAL_CONFIG.auth_config.password = input("")
-                api.login(GLOBAL_CONFIG.auth_config.username,
-                          GLOBAL_CONFIG.auth_config.password,
-                          GLOBAL_CONFIG.auth_config.mfa_code)
-                GLOBAL_CONFIG.auth_config.key = api.api_key
-                if cli_login(GLOBAL_CONFIG.auth_config.username):
-                    api.user_key_write_to_config_file()
+            _login_with_key(api)
         if len(GLOBAL_CONFIG.auth_config.accounts) != 0:
             api.sockpuppets = {account[0]: UserInfo(
                 account[0], account[1], '') for account in GLOBAL_CONFIG.auth_config.accounts}
